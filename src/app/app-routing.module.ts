0dd0f9740f87d08dc843551c574122b8ce2cfa09
BAYONA GARAVITO, SEBASTIAN <sebastian.bayonag@alumnos.upm.es>
2023-10-08 05:37:49 +0200

    Basic routing for the app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { UserListComponent } from './user-list/user-list.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'users', component: UserListComponent },
+  { path: 'users/:id', component: UserProfileComponent },
+  { path: "**", redirectTo: "users" }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
